9a5892286e4f3a26cf66559c296eebd620fca2ce
Pradeepshaan <shaanramp@gmail.com>
2021-06-14 11:49:36 +0530

    Section 01 Commit

---
+++ src/Components/SectionTwo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const SectionTwo = () => {
+  return (
+    <div style={{ background: "white" }}>
+      <h1>Testamony</h1>
+    </div>
+  );
+};
+
+export default SectionTwo;
